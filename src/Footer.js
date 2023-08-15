e9b7910f1c75a62a6f479778428215d4589cf1f8
Petrate <128822730+devpetrate@users.noreply.github.com>
2023-08-15 04:57:28 +0100

    Little Lemon Update 0.0.7

--- src/Footer.js
+++ src/Footer.js
@@ -1,29 +1,32 @@
 import Flogo from './icons/Ui kit (1).png'
+import './Footer.css'
 
 function Footer() {
     return (
         <>
-            <img src={Flogo} alt="Footer Logo" />
-            <section>
-                <h3>Doormat navigation</h3>
-                <p>Home</p>
-                <p>About</p>
-                <p>Menu</p>
-                <p>Reservation</p>
-                <p>Order Online</p>
-                <p>Login</p>
-            </section>
-            <section>
-                <h3>Contact</h3>
-                <p>Address</p>
-                <p>Phone number</p>
-                <p>Email</p>
-            </section>
-            <section>
-                <h3>Social Media Links</h3>
-                <p>Address</p>
-                <p>Phone number</p>
-                <p>Email</p>
+            <section className='sectf'>
+                <img className='imgf' src={Flogo} alt="Footer Logo" />
+                <section className='sectf1'>
+                    <h3 className='h3f'>Doormat navigation</h3>
+                    <p className='pf'>Home</p>
+                    <p className='pf'>About</p>
+                    <p className='pf'>Menu</p>
+                    <p className='pf'>Reservation</p>
+                    <p className='pf'>Order Online</p>
+                    <p className='pf'>Login</p>
+                </section>
+                <section className='sectf1'>
+                    <h3 className='h3f'>Contact</h3>
+                    <p className='pf'>Address</p>
+                    <p className='pf'>Phone number</p>
+                    <p className='pf'>Email</p>
+                </section>
+                <section className='sectf1'>
+                    <h3 className='h3f'>Social Media Links</h3>
+                    <p className='pf'>Address</p>
+                    <p className='pf'>Phone number</p>
+                    <p className='pf'>Email</p>
+                </section>
             </section>
         </>
     )
