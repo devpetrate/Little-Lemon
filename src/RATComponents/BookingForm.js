2552a6d8e2a8f06b524b4d1f0f3606fd7489d6b9
Petrate <128822730+devpetrate@users.noreply.github.com>
2023-08-16 23:55:12 +0100

    Little Lemon Update 0.0.16

--- src/RATComponents/BookingForm.js
+++ src/RATComponents/BookingForm.js
@@ -29,27 +29,28 @@ export function BookingForm() {
                     <section className="wrapper">
                         <label className="lab1">Reserve A Table</label>
                         <label htmlFor="res-name" className="lab2">Name:</label>
-                        <input className="inp1" type="text" id="res-name" placeholder="John Doe" required value={name} onChange={(e) => setName(e.target.value)} />
+                        <fieldset className="inp111"><input className="inp1" type="text" id="res-name" placeholder="John Doe" required value={name} onChange={(e) => setName(e.target.value)} /></fieldset>
                         <label htmlFor="res-email" className="lab2">Email:</label>
-                        <input className="inp1" type="email" id="res-email" placeholder="example@mail.com" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <fieldset className="inp111"><input className="inp1" type="email" id="res-email" placeholder="example@mail.com" required value={email} onChange={(e) => setEmail(e.target.value)} /></fieldset>
                         <label htmlFor="res-date" className="lab2">Choose date:</label>
-                        <input className="inp1 inp11" type="date" id="res-date" required value={date} onChange={(e) => setDate(e.target.value)} />
+                        <fieldset className="inp111"><input className="inp1 inp11" type="date" id="res-date" required value={date} onChange={(e) => setDate(e.target.value)} /></fieldset>
                         <label htmlFor="res-time" className="lab2">Choose time:</label>
-                        <select className="inp1" id="res-time" value={availableTimes} onChange={(e) => setAvailableTimes(e.target.value)}>
+                        <fieldset className="inp111"><select className="inp1" id="res-time" value={availableTimes} onChange={(e) => setAvailableTimes(e.target.value)}>
                             <option value="17:00">17:00</option>
                             <option value="18:00">18:00</option>
                             <option value="19:00">19:00</option>
                             <option value="20:00">20:00</option>
                             <option value="21:00">21:00</option>
                             <option value="22:00">22:00</option>
-                        </select>
+                        </select></fieldset>
                         <label htmlFor="guests" className="lab2">Number Of Guests:</label>
-                        <input className="inp1" type="number" placeholder="1-4" min="1" max="4" id="guests" required value={guests} onChange={(e) => setGuests(e.target.value)} />
+                        <fieldset className="inp111"><input className="inp1" type="number" placeholder="1-4" min="1" max="4" id="guests" required value={guests} onChange={(e) => setGuests(e.target.value)} /></fieldset>
                         <label htmlFor="occasion" className="lab2">Occasion:</label>
-                        <select className="inp1" id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+                        <fieldset className="inp111"><select className="inp1" id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
                             <option value="Birthday">Birthday</option>
                             <option value="Anniversary">Anniversary</option>
-                        </select></section>
+                        </select></fieldset>
+                    </section>
                     <section className="bntsectform">
                         <button disabled={!name || !email || !date || !guests} type="submit" className="but1">Make Your reservation</button></section>
 
