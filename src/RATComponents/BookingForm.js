e11a3cf1bf3367983b28578b58b1ac5e4c7fa910
Petrate <128822730+devpetrate@users.noreply.github.com>
2023-08-16 18:22:27 +0100

    Little Lemon Update 0.0.14

--- src/RATComponents/BookingForm.js
+++ src/RATComponents/BookingForm.js
@@ -29,13 +29,13 @@ export function BookingForm() {
                     <section className="wrapper">
                         <label className="lab1">Reserve A Table</label>
                         <label htmlFor="res-name" className="lab2">Name:</label>
-                        <input type="text" id="res-name" placeholder="John Doe" required value={name} onChange={(e) => setName(e.target.value)} />
+                        <input className="inp1" type="text" id="res-name" placeholder="John Doe" required value={name} onChange={(e) => setName(e.target.value)} />
                         <label htmlFor="res-email" className="lab2">Email:</label>
-                        <input type="email" id="res-email" placeholder="example@mail.com" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input className="inp1" type="email" id="res-email" placeholder="example@mail.com" required value={email} onChange={(e) => setEmail(e.target.value)} />
                         <label htmlFor="res-date" className="lab2">Choose date:</label>
-                        <input type="date" id="res-date" required value={date} onChange={(e) => setDate(e.target.value)} />
+                        <input className="inp1 inp11" type="date" id="res-date" required value={date} onChange={(e) => setDate(e.target.value)} />
                         <label htmlFor="res-time" className="lab2">Choose time:</label>
-                        <select id="res-time" value={availableTimes} onChange={(e) => setAvailableTimes(e.target.value)}>
+                        <select className="inp1" id="res-time" value={availableTimes} onChange={(e) => setAvailableTimes(e.target.value)}>
                             <option value="17:00">17:00</option>
                             <option value="18:00">18:00</option>
                             <option value="19:00">19:00</option>
@@ -44,9 +44,9 @@ export function BookingForm() {
                             <option value="22:00">22:00</option>
                         </select>
                         <label htmlFor="guests" className="lab2">Number Of Guests:</label>
-                        <input type="number" placeholder="1-4" min="1" max="4" id="guests" required value={guests} onChange={(e) => setGuests(e.target.value)} />
+                        <input className="inp1" type="number" placeholder="1-4" min="1" max="4" id="guests" required value={guests} onChange={(e) => setGuests(e.target.value)} />
                         <label htmlFor="occasion" className="lab2">Occasion:</label>
-                        <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+                        <select className="inp1" id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
                             <option value="Birthday">Birthday</option>
                             <option value="Anniversary">Anniversary</option>
                         </select></section>
